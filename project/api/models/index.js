4c9ef7b64b400f2608c7570d559d96ce92ba3cdf
dplamenov <dimitar.plamenov@gmail.com>
2020-12-10 17:35:12 +0200

    create index file in models directory
    create index file in models directory

--- project/api/models/index.js
+++ project/api/models/index.js
@@ -1,27 +1,3 @@
-const {Schema, model} = require('mongoose');
-
-const schema = new Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  realName: {
-    type: String,
-    required: true
-  },
-  dateOfBirth: {
-    type: String,
-    required: true
-  },
-  level: {
-    type: Number,
-    required: true,
-    default: 1
-  }
-});
-
-module.exports = model('user', schema);
+module.exports = {
+  User: require('./User')
+}
