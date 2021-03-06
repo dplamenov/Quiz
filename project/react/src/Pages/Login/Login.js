6c6490af08c91b7cf2f5d63e94288f8217623b86
dplamenov <dimitar.plamenov@gmail.com>
2021-03-06 10:18:46 +0200

    login form submit handler
    login form submit handler

--- project/react/src/Pages/Login/Login.js
+++ project/react/src/Pages/Login/Login.js
@@ -2,13 +2,34 @@ import React, {Component} from "react";
 import './Login.css';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    changeHandler(event) {
+        this.setState({[event.target.id]: event.target.value});
+    }
+
+    submitHandler(e) {
+        e.preventDefault();
+    }
+
     render() {
+        const {email, password} = this.state;
+
         return (
             <div className="wrapper login-page">
                 <h1>Login</h1>
-                <form className="login-form">
-                    <input type="text" name="email" id="password" placeholder="EMAIL"/>
-                    <input type="password" name="password" id="password" placeholder="PASSWORD"/>
+                <form className="login-form" onSubmit={this.submitHandler}>
+                    <input type="text" name="email" id="email" placeholder="EMAIL" value={email}
+                           onChange={this.changeHandler.bind(this)}/>
+                    <input type="password" name="password" id="password" placeholder="PASSWORD" value={password}
+                           onChange={this.changeHandler.bind(this)}/>
                     <button className="btn login-button">Login</button>
                 </form>
             </div>
