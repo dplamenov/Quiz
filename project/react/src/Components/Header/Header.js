a1ad6bfa5f06f8f5e6ec034a8aa731dc18a3b527
dplamenov <dimitar.plamenov@gmail.com>
2021-03-06 10:03:54 +0200

    update header nav links
    update header nav links

--- project/react/src/Components/Header/Header.js
+++ project/react/src/Components/Header/Header.js
@@ -11,8 +11,8 @@ class Header extends React.Component {
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/user/login">Login</Link></li>
+                    <li><Link to="/user/register">Register</Link></li>
                 </ul>
             </nav>
         </header>;
