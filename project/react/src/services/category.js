a2cc95bba3244c2b9c606521a406bf24babf75c8
dplamenov <dimitar.plamenov@gmail.com>
2021-03-09 19:21:10 +0200

    category service implement getAll method
    category service implement getAll method

--- project/react/src/services/category.js
+++ project/react/src/services/category.js
@@ -1,3 +1,14 @@
-const categoryService = {};
+import config from '../config/index';
+
+const categoryService = {
+    getAll() {
+        return fetch(`${config.apiUrl}category`, {
+            credentials: 'include', headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json());
+    }
+};
 
 export default categoryService;
