cec20343faf56839370a72cbb554401d96f03000
dplamenov <dimitar.plamenov@gmail.com>
2021-03-10 16:42:04 +0200

    create action types for register
    create action types for register

--- project/react/src/store/actions.js
+++ project/react/src/store/actions.js
@@ -3,6 +3,10 @@ export const ActionTypes = {
     LoginSuccess: Symbol("[AUTH] Login Success"),
     LoginFailure: Symbol("[AUTH] Login Failure"),
 
+    Register: Symbol("[AUTH] Register"),
+    RegisterSuccess: Symbol("[AUTH] Register Success"),
+    RegisterFailure: Symbol("[AUTH] Register Failure"),
+
     Logout: Symbol("[AUTH] Logout"),
     LogoutSuccess: Symbol("[AUTH] Logout Success"),
     LogoutFailure: Symbol("[AUTH] Logout Failure")
@@ -11,6 +15,10 @@ export const ActionTypes = {
 export const login = (user, cb, errorCb) => ({type: ActionTypes.Login, payload: {user, cb, errorCb}});
 export const loginFailure = (error) => ({type: ActionTypes.LoginFailure, payload: {error}});
 export const loginSuccess = (user) => ({type: ActionTypes.LoginSuccess, payload: {user}});
+
+export const register = (data, cb, errorCb) => ({type: ActionTypes.Register, payload: {data, cb, errorCb}});
+export const registerFailure = (error) => ({type: ActionTypes.RegisterFailure, payload: {error}});
+export const registerSuccess = (data) => ({type: ActionTypes.RegisterFailure, payload: {data}});
 
 export const logout = (cb) => ({type: ActionTypes.Logout, payload: {cb}});
 export const logoutFailure = (error) => ({type: ActionTypes.LogoutFailure, payload: {error}});
