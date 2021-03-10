612ab80b013d4625490266cd325f43a1d91977b0
dplamenov <dimitar.plamenov@gmail.com>
2021-03-10 15:32:14 +0200

    create validation helper
    create validation helper

--- project/react/src/User/Login/Login.js
+++ project/react/src/User/Login/Login.js
@@ -12,13 +12,18 @@ function Login(props) {
     const {dispatch} = React.useContext(StoreContext);
     const history = useHistory();
 
-    const emailChangeHandler = (event) => {
-        setEmail(event.target.value);
-
+    const emailChangeHandler = ({target}) => {
+        setEmail(target.value);
     }
+
     const passwordChangeHandler = (event) => {
         setPassword(event.target.value);
+    }
 
+    const validationHandler = (...validations) => {
+        return ({target}) => {
+            console.log(target, target.value);
+        }
     }
 
     const submitHandler = (e) => {
@@ -36,10 +41,11 @@ function Login(props) {
             <h1>Login</h1>
             <form className="login-form" onSubmit={submitHandler}>
                 {!!errorMessage ? <p>{errorMessage}</p> : ''}
+                <p>email is not valid</p>
                 <input type="text" name="email" id="email" placeholder="EMAIL" value={email}
-                       onChange={emailChangeHandler}/>
+                       onChange={emailChangeHandler} onBlur={validationHandler('email')}/>
                 <input type="password" name="password" id="password" placeholder="PASSWORD" value={password}
-                       onChange={passwordChangeHandler}/>
+                       onChange={passwordChangeHandler} onBlur={validationHandler}/>
                 <button className="btn login-button">Login</button>
             </form>
         </div>
