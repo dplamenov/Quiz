a3e2df4935a9b6547109d4e5f2c21d2837c6e686
dplamenov <dimitar.plamenov@gmail.com>
2021-03-11 16:18:57 +0200

    styling question component
    styling question component

--- project/react/src/Question/Question/Question.js
+++ project/react/src/Question/Question/Question.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./Question.css";
 
 function Question() {
-    return <p>question component</p>;
+    return (
+        <>
+            <h1 className="question-title">
+                When bulgaria celebrate christmas?
+            </h1>
+            <section className="answers">
+                <article className="question">
+                    1
+                </article>
+                <article className="question">
+                    2
+                </article>
+                <article className="question">
+                    3
+                </article>
+                <article className="question">
+                    4
+                </article>
+            </section>
+        </>
+    );
 }
 
 export default Question;
