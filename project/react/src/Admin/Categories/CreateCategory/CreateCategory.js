5e7e51c0201a560a2cc914c479c3d0198fe32ff9
dplamenov <dimitar.plamenov@gmail.com>
2021-03-16 16:15:17 +0200

    handler for create category form
    handler for create category form

--- project/react/src/Admin/Categories/CreateCategory/CreateCategory.js
+++ project/react/src/Admin/Categories/CreateCategory/CreateCategory.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './CreateCategory.css';
 
 function CreateCategory() {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const nameChangeHandler = ({target}) => {
+        setName(target.value);
+    }
+
+    const descriptionChangeHandler = ({target}) => {
+        setDescription(target.value);
+    }
+
+
+    const createCategoryHandler = (e) => {
+        e.preventDefault();
+        console.log({name, description})
+    }
+
     return (
         <>
             <h2 className="admin-panel-categories-create-heading">Create category</h2>
-            <form>
-                <input type="text" className="input" placeholder="CATEGORY NAME"/>
-                <textarea placeholder="CATEGORY DESCRIPTION" className="input no-resize" rows={10}/>
+            <form onSubmit={createCategoryHandler}>
+                <input type="text" className="input" placeholder="CATEGORY NAME" value={name}
+                       onChange={nameChangeHandler}/>
+                <textarea placeholder="CATEGORY DESCRIPTION" className="input no-resize"
+                          rows={10} value={description} onChange={descriptionChangeHandler}/>
                 <button className="btn">Create</button>
             </form>
         </>
