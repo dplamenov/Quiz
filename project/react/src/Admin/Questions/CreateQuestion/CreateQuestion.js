ea65485b8dd5a185e5734526e23632e68cefb0a9
dplamenov <dimitar.plamenov@gmail.com>
2021-03-17 14:51:18 +0200

    create `create question` component
    create `create question` component

---
+++ project/react/src/Admin/Questions/CreateQuestion/CreateQuestion.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CreateQuestion.css';
+
+function CreateQuestions() {
+    return (
+        <>
+            <h2>Create question</h2>
+        </>
+    );
+}
+
+export default CreateQuestions;
