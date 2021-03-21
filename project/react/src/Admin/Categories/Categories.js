d073d0c0b0b3be4db25cb51a45f871c1a062133b
dplamenov <dimitar.plamenov@gmail.com>
2021-03-21 10:21:11 +0200

    styling delete category component
    styling delete category component

--- project/react/src/Admin/Categories/Categories.js
+++ project/react/src/Admin/Categories/Categories.js
@@ -2,17 +2,27 @@ import React, {useEffect, useState} from "react";
 import adminService from "../../services/admin";
 import './Categories.css';
 import {Link} from "react-router-dom";
+import DeleteCategory from "./DeleteCategory/DeleteCategory";
+import category from "../../services/category";
 
 function Categories({history}) {
     const [categories, setCategories] = useState([]);
+    const [deleteCategory, setDeleteCategory] = useState(false);
 
     useEffect(() => {
         adminService.getAllCategories()
             .then(categories => setCategories(categories.sort((u1, u2) => u1.id - u2.id)));
     }, []);
 
+    const deleteHandler = (catId) => {
+        return () => {
+            setDeleteCategory(catId);
+        };
+    };
+
     return (
         <>
+            {deleteCategory ? <DeleteCategory id={deleteCategory}/> : ''}
             <h1>Categories</h1>
             <section className="admin-panel-categories-actions">
                 <button className="btn" onClick={() => history.push('/admin/categories/create')}>
@@ -35,7 +45,7 @@ function Categories({history}) {
                             <td>{cat.id}</td>
                             <td>{cat.name}</td>
                             <td><Link to={'category/' + cat.id + '/edit'}>Edit</Link></td>
-                            <td>Delete</td>
+                            <td onClick={deleteHandler(cat.id)}>Delete</td>
                         </tr>
                     );
                 })}
--- project/react/src/Admin/Categories/DeleteCategory/DeleteCategory.js
+++ project/react/src/Admin/Categories/DeleteCategory/DeleteCategory.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import 'DeleteCategory.css';
+import './DeleteCategory.css';
 
-function DeleteCategory() {
+function DeleteCategory({id}) {
     return (
         <section className="delete-category">
             <h1 className="delete-category-heading">Are you sure?</h1>
-            <p className="delete-category-text">You will delete category _____ and all X question(s) in this
-                category.</p>
+            <p className="delete-category-text">
+                You will delete category {id} and all X question(s) in this
+                category.
+            </p>
             <article className="delete-category-image-wrapper">
                 <img src="/images/warning.png" alt=""/>
+            </article>
+            <article className="delete-category-button-wrapper">
+                <button className="btn">Yes</button>
+                <button className="btn">No</button>
             </article>
         </section>
     );
