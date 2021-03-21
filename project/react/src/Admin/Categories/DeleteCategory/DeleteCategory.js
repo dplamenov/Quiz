2b6c809bc4544467510a76b1f2bbb16a29fdfc0a
dplamenov <dimitar.plamenov@gmail.com>
2021-03-21 11:37:36 +0200

    delete category text
    delete category text

--- project/react/src/Admin/Categories/DeleteCategory/DeleteCategory.js
+++ project/react/src/Admin/Categories/DeleteCategory/DeleteCategory.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './DeleteCategory.css';
+import categoryService from "../../../services/category";
 
 function DeleteCategory({id, closeHandler}) {
+    const [category, setCategory] = useState();
+
+    useEffect(() => {
+        categoryService.getById(id)
+            .then(category => {
+                setCategory(category);
+            });
+    }, [categoryService]);
 
     const noHandler = () => {
         closeHandler();
+    };
+
+    const endMessage = (qCount) => {
+        if (qCount === 0) {
+            return '.';
+        }
+
+        if (qCount === 1) {
+            return  ' and 1 question in category.';
+        }
+
+        return  ` and ${qCount} questions in category.`;
     };
 
     return (
         <section className="delete-category">
             <h1 className="delete-category-heading">Are you sure?</h1>
             <p className="delete-category-text">
-                You will delete category {id} and all X question(s) in this
-                category.
+                You will delete category {category?.name}{endMessage(category?.questionsCount)}
             </p>
             <article className="delete-category-image-wrapper">
                 <img src="/images/warning.png" alt=""/>
