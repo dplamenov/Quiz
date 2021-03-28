315b8e5ba46b6759399f4da4ff02fdc22ae6fbb1
dplamenov <dimitar.plamenov@gmail.com>
2021-03-28 12:31:10 +0300

    update footer navigation
    update footer navigation

--- project/react/src/Core/Footer/Footer.js
+++ project/react/src/Core/Footer/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Footer.css';
+import {Link} from "react-router-dom";
 
 function Footer (){
     return (
-      <footer>Footer</footer>
+      <footer>
+          <nav>
+              <ul>
+                  <li><Link to="/contact">Contact us</Link></li>
+              </ul>
+          </nav>
+      </footer>
     );
 }
 
