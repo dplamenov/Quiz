9da64489eaed407075945531cab98a1c869c0d41
dplamenov <dimitar.plamenov@gmail.com>
2021-04-02 18:23:42 +0300

    contact form bug fix
    contact form bug fix

--- project/react/src/Core/Contact/Contact.js
+++ project/react/src/Core/Contact/Contact.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './Contact.css';
 import {toast} from "react-toastify";
 import Title from "../Title";
+import {StoreContext} from "../../store/store";
 
 function Contact({history}) {
+    const {state: {user}} = React.useContext(StoreContext);
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -16,9 +18,9 @@ function Contact({history}) {
             <Title>Contact</Title>
             <h1>Contact</h1>
             <div className="wrapper">
-                <form onClick={submitHandler}>
-                    <input type="text" className="input" placeholder="Your name"/>
-                    <input type="text" className="input" placeholder="Email"/>
+                <form onSubmit={submitHandler}>
+                    <input type="text" className="input" placeholder="Your name" value={user?.real_name}/>
+                    <input type="text" className="input" placeholder="Email" value={user?.email}/>
                     <textarea cols="30" rows="10" className="input no-resize" placeholder="Message"/>
                     <button className="btn">Contact</button>
                 </form>
