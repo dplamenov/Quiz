6b7b739a70b6ba709960823a45363ef1c8297120
dplamenov <dimitar.plamenov@gmail.com>
2021-10-09 17:03:16 +0300

    Update CRM.js

--- project/react/src/Admin/CRM/CRM.js
+++ project/react/src/Admin/CRM/CRM.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 
 function CRM() {
-    return <p>crm</p>;
+    return (
+        <>
+            <h2>Customer relationship management</h2>
+            <NavLink to={`/admin/crm/email-marketing`}>Email marketing</NavLink>
+        </>
+    );
 }
 
 export default CRM;
